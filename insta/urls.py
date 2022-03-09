4611820141e6784b09939f6a605cc2cbe0f6bd73
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-03-09 11:29:58 +0300

    Changes

--- insta/urls.py
+++ insta/urls.py
@@ -10,6 +10,7 @@ urlpatterns=[
     path('create/profile/', views.create_profile, name = 'create_profile'),
     path('profile/<str:username>/', views.profile, name = 'profile'),
     path('search/username/', views.search_profile, name= 'search_profile'),
+    path('comments/image/<int:id>/', views.comment, name= 'comment'),
     path('view/profile/<str:username>', views.view_profile, name = 'viewProfile'),
     path('followToggle/<str:user>', views.followToggle, name = 'followToggle'),
     path('like/<int:id>', views.like, name='like'),
